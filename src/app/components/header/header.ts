20500a8c00cd383bbf7036d6e7668e9340707bc9
Gustavo Lopes <158600452+lopesgsl@users.noreply.github.com>
2025-07-14 01:16:40 -0300

    Update header with new logo and style improvements
    Replaced the header logo with a new image (logo-las.png), updated the header background to a gradient, and adjusted logo styling for better responsiveness. Updated Angular component to use NgOptimizedImage for improved image handling. Added assets and favicon to angular.json assets array. Also included VS Code workspace color settings.

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,15 @@
 // Ficheiro: src/app/components/header/header.ts
 
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true, // <-- AQUI ESTÁ A CORREÇÃO!
-  imports: [],
+  imports: [ 
+    CommonModule,
+    NgOptimizedImage
+  ],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
