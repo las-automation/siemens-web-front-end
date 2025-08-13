f708a0201adbde25834fa2cf4d58bf5f957e8e86
Gustavo Lopes <158600452+lopesgsl@users.noreply.github.com>
2025-08-13 12:25:25 -0300

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,20 @@
+// src/app/app.config.ts
+
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 import { provideHttpClient, withFetch } from '@angular/common/http'; 
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+// 1. ALTERE ESTA LINHA: remova o '/async' do final
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideRouter(routes), 
-    provideAnimationsAsync(), 
+    provideRouter(routes), // <-- REMOVI A LINHA DUPLICADA
+    provideAnimations(),   // 2. ALTERE ESTA LINHA: remova o 'Async' do nome
     provideHttpClient(withFetch()),
   ]
 };
